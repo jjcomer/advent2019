3f671ab12a63a970bbe4a0e0ed17c2bbe3af2a48
Josh Comer <jcomer@cvent.com>
2019-12-04 20:10:36 -0400

    Day 4 -- part 2

--- src/days/day_four.rs
+++ src/days/day_four.rs
@@ -8,12 +8,30 @@ fn split_number(password: i32) -> Vec<i8> {
         .collect()
 }
 
-fn pair_test(password: &[i8], (i1, i2): &(usize, usize)) -> bool {
-    password[*i1] == password[*i2]
-}
+// fn pair_test(password: &[i8], (i1, i2): &(usize, usize)) -> bool {
+//     password[*i1] == password[*i2]
+// }
 
-fn check_pairs(password: &[i8]) -> bool {
-    (0..5).zip(1..6).any(|x| pair_test(password, &x))
+// fn check_pairs(password: &[i8]) -> bool {
+//     (0..5).zip(1..6).any(|x| pair_test(password, &x))
+// }
+
+fn check_long_pairs(password: &[i8]) -> bool {
+    let mut count = 1;
+    let mut last = -1;
+
+    for p in password {
+        if last == *p {
+            count += 1;
+        } else {
+            if count == 2 {
+                return true;
+            }
+            count = 1;
+        }
+        last = *p;
+    }
+    count == 2
 }
 
 fn ascending_test(password: &[i8], (i1, i2): &(usize, usize)) -> bool {
@@ -30,7 +48,8 @@ fn check_ascending(password: &[i8]) -> bool {
 pub fn process() -> Result<()> {
     let passwords = (387_638..919_123)
         .map(split_number)
-        .filter(|x| check_pairs(x))
+        //.filter(|x| check_pairs(x))
+        .filter(|x| check_long_pairs(x))
         .filter(|x| check_ascending(x))
         .count();
 

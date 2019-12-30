6dbaa0f4ba395d2a47808a75eaa8e672fbd86b69
Josh Comer <joshjcomer@gmail.com>
2019-12-30 06:33:23 -0400

    WIP day12

--- src/day12.rs
+++ src/day12.rs
@@ -1,19 +1,65 @@
 use regex::Regex;
 
-pub type Coord = Vec<(i64, i64, i64)>;
+#[derive(Default, PartialEq, Eq, Debug)]
+pub struct Body {
+    velocity: (i64, i64, i64),
+    position: (i64, i64, i64),
+}
 
+impl std::ops::Add for Body {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        let velocity = (
+            self.velocity.0 + other.velocity.0,
+            self.velocity.1 + other.velocity.1,
+            self.velocity.2 + other.velocity.2,
+        );
+        Self {
+            velocity,
+            position: self.position,
+        }
+    }
+}
+
+pub type Coord = Vec<Body>;
+//<x=-1, y=0, z=2>
 pub fn generate_input(input: &str) -> Coord {
-    let reg = Regex::new(r"^<x=(\d+), y=(\d+), z=(\d+)>$").unwrap();
+    let reg = Regex::new(r"^<x=([-\d]+), y=([-\d]+), z=([-\d]+)>$").unwrap();
 
     input
         .lines()
         .map(|l| {
             let matches = reg.captures(l).unwrap();
-            (
+            let velocity = (
                 matches[1].parse::<i64>().unwrap(),
                 matches[2].parse::<i64>().unwrap(),
                 matches[3].parse::<i64>().unwrap(),
-            )
+            );
+            Body {
+                velocity,
+                ..Default::default()
+            }
         })
         .collect()
+}
+
+fn solve_part1(input: &Coord) -> i64 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example1() {
+        let input = "<x=-1, y=0, z=2>";
+        assert_eq!(
+            generate_input(&input),
+            vec![Body {
+                velocity: (-1, 0, 2),
+                ..Default::default()
+            }]
+        );
+    }
 }
